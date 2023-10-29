a922f14b9846e589580448d1a0be005b8580c881
MtFBella109 <130922446+MtFBella109@users.noreply.github.com>
2023-10-29 21:17:12 +0100

    Add files via upload

---
+++ dev/packages.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    appimage-run
+    brave
+    cmatrix
+    discord
+    easyeffects
+    helvum
+    htop
+    libnotify
+    lutris
+    onefetch
+    onlyoffice-bin
+    pcmanfm
+    polkit_gnome
+    protonup-qt
+    spotify
+    steam
+    swww
+    tmux
+    tree
+    vlc
+    vscode-with-extensions
+  ];
+}
