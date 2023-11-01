fd49588b80e7e1cc1ed3f4f992e00d95f95a901e
Bella <130922446+MtFBella109@users.noreply.github.com>
2023-11-01 17:16:25 +0100

    Update default.nix

--- home/user/dev/default.nix
+++ home/user/dev/default.nix
@@ -1,20 +1,14 @@
 _: {
   imports = [
     ./c
-    # ./clojure
     ./compilers
-    # ./dart
     ./editors
-    # ./elixir
     ./go
     ./js
     ./lua
     ./nix
-    ./python
     ./rust
     ./dconf
-    # ./docker.nix
-    #./virt-manager
     ./packages.nix
   ];
 }
