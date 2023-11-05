3852993bf21d4873ff0024915a36df4b43296799
Bella <130922446+MtFBella109@users.noreply.github.com>
2023-11-05 14:37:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,11 +39,12 @@ fi
 rm -rf ./.git
 
 ## Make a symlink to the dotfiles, that aren't managed via home-manager
-ln -s ./dots/config/hypr/autostart $HOME/.config/hypr/autostart
-ln -s ./dots/config/hypr/scripts $HOME/.config/hypr/scripts
-ln -s ./dots/config/hypr/store $HOME/.config/hypr/store
-ln -s ./dots/backgrounds $HOME/.backgrounds
-ln -s ./dots/themes $HOME/.themes
+mkdir ~/.config/hypr
+ln -s ./dots/config/hypr/autostart ~/.config/hypr/autostart
+ln -s ./dots/config/hypr/scripts ~/.config/hypr/scripts
+ln -s ./dots/config/hypr/store ~/.config/hypr/store
+ln -s ./dots/backgrounds ~/.backgrounds
+ln -s ./dots/themes ~/.themes
 
 ## Copy the hardware-configuration.nix
 echo "Should we copy the hardware-configuration.nix from /etc/nixos to this directory y/N? (Needed to apply all the configs, this command will need sudo privileges)"
