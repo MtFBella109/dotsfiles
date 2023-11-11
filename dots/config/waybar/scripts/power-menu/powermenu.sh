6d2f34e622d8a072838d19045e60a6725ff4c405
Bella <130922446+MtFBella109@users.noreply.github.com>
2023-11-11 17:18:00 +0100

    Update powermenu.sh

--- dots/config/waybar/scripts/power-menu/powermenu.sh
+++ dots/config/waybar/scripts/power-menu/powermenu.sh
@@ -18,13 +18,13 @@ uptime="$(uptime -p | sed -e 's/up //g')"
 host=$(hostname)
 
 # Options
-shutdown=' Shutdown'
-reboot=' Reboot'
-lock=' Lock'
-suspend=' Suspend'
-logout=' Logout'
-yes=' Yes'
-no=' No'
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend=' Suspend'
+logout=' Logout'
+yes=' Yes'
+no=' No'
 
 # Prevent release key not getting registered
 sleep 0.05
