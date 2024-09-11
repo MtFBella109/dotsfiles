b091c0b9f756b3de7fce6ae058299cfea7bf63e9
Bella <130922446+MtFBella109@users.noreply.github.com>
2024-09-11 15:41:45 +0200

    Removed not used fonts

--- home/user/system/fonts/default.nix
+++ home/user/system/fonts/default.nix
@@ -7,24 +7,16 @@
 }: {
   home.packages = with pkgs; [
     # Fonts
-    # go-font
     apl386
     corefonts
     nerdfonts
     noto-fonts
     dejavu_fonts
     font-awesome
-    # maple-mono-NF
-    # courier-prime
-    # liberation_ttf # Needed to make vanilla st terminal work
-    # powerline-fonts
-    # cantarell-fonts
-    # source-code-pro
     fira-code-symbols
     powerline-symbols
     material-design-icons
     vistafonts
-    # helvetica-neue-lt-std
     (nerdfonts.override {fonts = ["IBMPlexMono" "CascadiaCode" "FiraCode" "JetBrainsMono" "LiberationMono" "SourceCodePro" "Iosevka"];})
   ];
 }
